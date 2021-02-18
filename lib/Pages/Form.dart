530aaab0d428745d59377b0a300beb9cf8726168
Valentin Bissay <60673239+Valentinbis@users.noreply.github.com>
2021-02-18 11:08:22 +0100

    accueil

---
+++ lib/Pages/Form.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Form extends StatelessWidget {
+  
+}
---
+++ lib/Pages/Home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar : AppBar(
+        title: Text("Accueil"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _buttonForm(),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _buttonForm() {
+    return new Center(
+      child : RaisedButton(
+        onPressed: () {
+          // TODO faire action
+        },
+        child: Text('Formulaire'),
+      ),
+    ),
+  }
+}
