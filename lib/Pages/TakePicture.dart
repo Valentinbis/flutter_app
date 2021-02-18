66936f423bc2a19c2fda7b47007d0212f0a4d10e
Zgreg <greg.dilon@hotmail.fr>
2021-02-18 14:05:53 +0100

    Intégration appareil photo

--- lib/Pages/TakePicture.dart
+++ lib/Pages/TakePicture.dart
@@ -1,11 +1,43 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class TakePicture extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class TakePicture extends StatefulWidget {
+  const TakePicture({Key key}) : super(key: key);
+  @override
+  _TakePictureState createState() => _TakePictureState();
+}
+
+class _TakePictureState extends State<TakePicture> {
+  File _image;
+  final picker = ImagePicker();
+
+  Future _getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Appareil Photo'),
+        title: Text('Prend une photo'),
+      ),
+      body: Center(
+        child: _image == null ? Text('No image selected.') : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
