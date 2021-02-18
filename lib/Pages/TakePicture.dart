66f77a5e8742d435ff4640183df9f0981d3a95d0
Zgreg <greg.dilon@hotmail.fr>
2021-02-18 14:20:52 +0100

    Appareil photo fonctionnel

--- lib/Pages/TakePicture.dart
+++ lib/Pages/TakePicture.dart
@@ -5,6 +5,7 @@ import 'package:image_picker/image_picker.dart';
 
 class TakePicture extends StatefulWidget {
   const TakePicture({Key key}) : super(key: key);
+
   @override
   _TakePictureState createState() => _TakePictureState();
 }
@@ -12,6 +13,23 @@ class TakePicture extends StatefulWidget {
 class _TakePictureState extends State<TakePicture> {
   File _image;
   final picker = ImagePicker();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Prend une photo'),
+      ),
+      body: Center(
+        child: _image == null ? Text('No image selected.') : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async => _getImage(),
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
 
   Future _getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
@@ -25,20 +43,44 @@ class _TakePictureState extends State<TakePicture> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Prend une photo'),
-      ),
-      body: Center(
-        child: _image == null ? Text('No image selected.') : Image.file(_image),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getImage,
-        tooltip: 'Pick Image',
-        child: Icon(Icons.add_a_photo),
-      ),
-    );
-  }
+  // File _imageFile;
+  // final _picker = ImagePicker();
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return ListView(
+  //     children: <Widget>[
+  //       ButtonBar(
+  //         children: <Widget>[
+  //           IconButton(
+  //             icon: const Icon(Icons.photo_camera),
+  //             onPressed: () async => _pickImageFromCamera(),
+  //             tooltip: 'Shoot picture',
+  //           ),
+  //           IconButton(
+  //             icon: const Icon(Icons.photo),
+  //             onPressed: () async => _pickImageFromGallery(),
+  //             tooltip: 'Pick from gallery',
+  //           ),
+  //         ],
+  //       ),
+  //       if (this._imageFile == null)
+  //         const Placeholder()
+  //       else
+  //         Image.file(this._imageFile),
+  //     ],
+  //   );
+  // }
+
+  // Future _pickImageFromGallery() async {
+  //   final PickedFile pickedFile =
+  //       await _picker.getImage(source: ImageSource.gallery);
+  //   setState(() => this._imageFile = File(pickedFile.path));
+  // }
+
+  // Future _pickImageFromCamera() async {
+  //   final PickedFile pickedFile =
+  //       await _picker.getImage(source: ImageSource.camera);
+  //   setState(() => this._imageFile = File(pickedFile.path));
+  // }
 }
